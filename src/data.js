f4f0cb5e3f946674c5c060ae993ed64186172402
ChaminiMaduwanthi <chaminimaduwanthi97gmail.com>
2025-01-18 16:59:34 +0530

    fix the features issue

---
+++ src/data.js
@@ -0,0 +1,36 @@
+import Img1 from "./assets/images/img-1.png";
+import Img2 from "./assets/images/img-2.jpg";
+import Img3 from "./assets/images/img3.png";
+
+export { Img1, Img2, Img3 };
+
+export const links = [
+  { name: "Home", path: "home" },
+  { name: "About", path: "about" },
+  { name: "Features", path: "features" },
+  { name: "Menu", path: "menu" },
+  { name: "Gallery", path: "gallery" },
+  { name: "Chef", path: "chef" },
+  { name: "Testimonials", path: "testimonials" },
+];
+
+export const features = [
+  {
+    img: Img1,
+    title: "Fast Delivery",
+    description:
+      "Experience lightning-fast delivery with our optimized logistics network.",
+  },
+  {
+    img: Img2,
+    title: "Quality Ingredients",
+    description:
+      "We ensure the highest quality ingredients for a delightful experience.",
+  },
+  {
+    img: Img3,
+    title: "Customizable Options",
+    description:
+      "Tailor our products and services to meet your specific needs.",
+  },
+];
